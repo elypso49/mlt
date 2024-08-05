1cccfd73978d36153f0e37454e288fb01c2c76e0
Elypso <giet.francois@gmail.com>
2024-08-05 12:04:17 +0200

    Creating control based api

---
+++ mlt.api/Controllers/BetaSerieController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace mlt.api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class BetaSerieController
+{
+    [HttpGet("shows")]
+    public ActionResult<string> Test()
+        => null;
+}
--- mlt.api/Program.cs
+++ mlt.api/Program.cs
@@ -3,40 +3,42 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 app.UseSwagger();
+
 app.UseSwaggerUI(options =>
                  {
                      options.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
                      options.RoutePrefix = string.Empty; // Serve Swagger UI at root path
                  });
 
+// var summaries = new[]
+//                 {
+//                     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+//                 };
+//
+// app.MapGet("/weatherforecast",
+//            () =>
+//            {
+//                var forecast = Enumerable.Range(1, 5)
+//                                         .Select(index => new WeatherForecast(DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+//                                                                              Random.Shared.Next(-20, 55),
+//                                                                              summaries[Random.Shared.Next(summaries.Length)]))
+//                                         .ToArray();
+//
+//                return forecast;
+//            })
+//    .WithName("GetWeatherForecast")
+//    .WithOpenApi();
 
-var summaries = new[]
-                {
-                    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-                };
-
-app.MapGet("/weatherforecast",
-           () =>
-           {
-               var forecast = Enumerable.Range(1, 5)
-                                        .Select(index => new WeatherForecast(DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                                                                             Random.Shared.Next(-20, 55),
-                                                                             summaries[Random.Shared.Next(summaries.Length)]))
-                                        .ToArray();
-
-               return forecast;
-           })
-   .WithName("GetWeatherForecast")
-   .WithOpenApi();
+app.MapControllers();
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+// record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+// {
+//     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+// }
